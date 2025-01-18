3b86cc74de346a4192de09d2efcb7ba36d2f0476
adham yehia <adhamyehia194@gmail.com>
2025-01-18 22:30:12 +0200

    Q1

---
+++ Assignment/Interfaces/ICircle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Interfaces
+{
+    interface ICircle : IShape
+    {
+        //public int Area { get; set; }
+
+        //public void DisplayShapeInfo()
+        //{
+        //    Console.WriteLine($"Rectangle Area is  {Area}");
+        //}
+
+
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Assignment
+﻿
+using Rectangle = Assignment.Classes.Rectangle;
+
+using Circle = Assignment.Classes.Circle;
+using Assignment.Interfaces;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1 - Define an interface named IShape with a property Area and a method DisplayShapeInfo.Create two interfaces, ICircle and IRectangle, that inheritfrom IShape.Implement these interfaces in classes Circle and Rectangle. Testyour implementation by creating instances of both classes and displaying theirshape information.
+
+            Rectangle R = new Rectangle();
+
+            R.Area = 10;
+
+            R.DisplayShapeInfo();
+
+            Circle Circle = new Circle();
+
+            Circle.Area = 25;
+
+            Circle.DisplayShapeInfo();
+
+            #endregion
+
+
+
         }
     }
 }
